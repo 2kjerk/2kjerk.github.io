0c3b781b8d6e2f1bbabe5931542d54be88101ece
2kjerk <evankeeran5@gmail.com>
2026-08-27 12:19:38 -0500

    Update library.js

--- js/library.js
+++ js/library.js
@@ -124,7 +124,7 @@ const MUSIC_LIBRARY = [
     coverFallback: null,
     durationText: '',
     tracks: [
-      { title: 'LOST IT (remaster)', src: 'music/2026/sipping drank & popping pills ft. tezzus.mp3' },
+      { title: 'sipping drank & popping pills ft. tezzus', src: 'music/2026/sipping drank & popping pills ft. tezzus.mp3' },
       { title: 'LOST IT (remaster)', src: 'music/2026/LOST IT REMASTER V1.3.mp3' },
       { title: 'how you feel?', src: 'music/2026/how you feel.mp3' },
       { title: 'lost love', src: 'music/2026/lost love.mp3' },
